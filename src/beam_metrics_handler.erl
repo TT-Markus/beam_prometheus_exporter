a7c117f0c3373d08093da665b624eab98c46d1c0
Markus Feyh <markus.feyh@gmail.com>
2019-01-31 12:44:19 -0800

    Cowboy 1 example prometheus

--- src/beam_metrics_handler.erl
+++ src/beam_metrics_handler.erl
@@ -1,30 +1,11 @@
 -module(beam_metrics_handler).
 
--export([init/2,
-         terminate/3]).
+-export([init/3, handle/2, terminate/3]).
 
-init(Req, Opts) ->
-  case is_authorized(Req) of
-    false ->
-      Req1 = cowboy_req:reply(401, #{
-                                <<"WWW-Authenticate">> => <<"Basic realm=\"prometheus\"">>
-                               }, Req),
-      {ok, Req1, Opts};
-    true ->
-      prometheus_cowboy2_handler:init(Req, Opts)
-  end.
+init(Transport, Req, Opts) ->
+	  prometheus_cowboy1_handler:init(Transport, Req, Opts).
 
-terminate(Reason, Req, State) ->
-  prometheus_cowboy2_handler:terminate(Reason, Req, State).
+handle(Req, State) ->
+	    prometheus_cowboy1_handler:handle(Req, State).
 
-%%% internal
-
-is_authorized(Req) ->
-  {ok, {Username, Password}} = application:get_env(beam_prometheus, auth),
-  Auth = cowboy_req:parse_header(<<"authorization">>, Req),
-  case Auth of
-    {basic, Username, Password} ->
-      true;
-    _ ->
-      false
-  end.
+terminate(_,_,_) -> ok.
--- src/beam_prometheus_app.erl
+++ src/beam_prometheus_app.erl
@@ -9,11 +9,15 @@
 %% API
 
 start(_StartType, _StartArgs) ->
-  Dispatch = cowboy_router:compile([{'_', [{"/metrics/[:registry]", beam_metrics_handler, []}]}]),
-  Port = application:get_env(beam_prometheus, port, 9101),
-  {ok, _} = cowboy:start_clear(my_http_listener,
-                               [{port, Port}],
-                                #{env => #{dispatch => Dispatch}}),
+		  Dispatch = cowboy_router:compile([
+		  	   {'_', [
+					{"/metrics/[:registry]", beam_metrics_handler, []}
+					      ]}
+					      ]),
+					      {ok, _} = cowboy:start_http(http, 100, [{port, 9101}], [
+					      	   {compress, true},
+						   	      {env, [{dispatch, Dispatch}]}
+							      ]),
   beam_prometheus_sup:start_link().
 
 stop(_State) ->
